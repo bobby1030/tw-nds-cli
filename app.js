0249a8130e9e55f3764f440333c982e8c0116912
BobbyHo <angela.bobby@gmail.com>
2016-09-15 13:59:14 +0800

    Finish Basic Feature

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const rp = require('request-promise')
 const cheerio = require('cheerio');
 const cheerioTableParser = require('cheerio-tableparser')
+const replaceLocationToISO = require('./replaceLocationToISO')
 
-rp('http://www.dgpa.gov.tw/nds.html')
-    .then(function(res){
-        parseHTML(res)
-    })
-    .catch(function (err) {
-        console.error('Error when fetching data')
-    });
+var fetchData = function() {
+    rp('http://www.dgpa.gov.tw/nds.html')
+        .then(function(res){
+            parseHTML(res)
+        })
+        .catch(function (err) {
+            console.error('Error when fetching data')
+        });
+}
+
 
 var parseHTML = function(rawHTMl){
     var $ = cheerio.load(rawHTMl)
@@ -19,13 +23,26 @@ var parseHTML = function(rawHTMl){
     var cities = data[0].slice(1)
     var status = data[1].slice(1)
 
-    console.log(mergeArray(cities, status));
+    result = mergeArray(cities, status)
+    resultHandler(result)
 }
 
 var mergeArray = function(column, data) {
     var obj = {}
     for(var i=0; i < column.length; i++){
-        obj[column[i]] = data[i]
+        obj[replaceLocationToISO(column[i])] = data[i]
     }
     return obj
 }
+
+var resultHandler = function(result){
+    if (result[process.argv[2]]) {
+        console.log(result[process.argv[2]]);
+    } else if (process.argv[2]){
+        console.error('Please use a City Code that match ISO 3166-2:TW format.')
+    } else {
+        console.error('Please specify a city with ISO 3166-2:TW format.')
+    }
+}
+
+fetchData()
