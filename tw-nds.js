72d3d09e47d94eaf7dd8d74a2a0ba43f4d55e43e
BobbyHo <angela.bobby@gmail.com>
2016-09-15 15:10:20 +0800

    Fix semicolon & beautify code

--- tw-nds.js
+++ tw-nds.js
@@ -1,48 +1,47 @@
-const rp = require('request-promise')
+const rp = require('request-promise');
 const cheerio = require('cheerio');
-const cheerioTableParser = require('cheerio-tableparser')
-const replaceLocationToISO = require('./replaceLocationToISO')
+const cheerioTableParser = require('cheerio-tableparser');
+const replaceLocationToISO = require('./replaceLocationToISO');
 
-var fetchData = function() {
+var fetchData = function () {
     rp('http://www.dgpa.gov.tw/nds.html')
-        .then(function(res){
-            parseHTML(res)
+        .then(function (res) {
+            parseHTML(res);
         })
         .catch(function (err) {
-            console.error('Error when fetching data')
+            console.error('Error when fetching data');
         });
-}
+};
 
-
-var parseHTML = function(rawHTMl){
-    var $ = cheerio.load(rawHTMl)
+var parseHTML = function (rawHTMl) {
+    var $ = cheerio.load(rawHTMl);
     cheerioTableParser($);
 
-    var data = $('table[bgcolor="#cdfad9"]').parsetable(false,false,true)
+    var data = $('table[bgcolor="#cdfad9"]').parsetable(false, false, true);
 
-    var cities = data[0].slice(1)
-    var status = data[1].slice(1)
+    var cities = data[0].slice(1);
+    var status = data[1].slice(1);
 
-    result = mergeArray(cities, status)
-    resultHandler(result)
-}
+    result = mergeArray(cities, status);
+    resultHandler(result);
+};
 
-var mergeArray = function(column, data) {
-    var obj = {}
-    for(var i=0; i < column.length; i++){
-        obj[replaceLocationToISO(column[i])] = data[i].replace('  ','\n')
-    }
-    return obj
-}
+var mergeArray = function (column, data) {
+    var obj = {};
+    for (var i = 0; i < column.length; i++) {
+        obj[replaceLocationToISO(column[i])] = data[i].replace('  ', '\n');
+    };
+    return obj;
+};
 
-var resultHandler = function(result){
+var resultHandler = function (result) {
     if (result[process.argv[2]]) {
         console.log(result[process.argv[2]]);
-    } else if (process.argv[2]){
-        console.error('Please use a City Code that match ISO 3166-2:TW format.')
+    } else if (process.argv[2]) {
+        console.error('Please use a City Code that match ISO 3166-2:TW format.');
     } else {
-        console.error('Please specify a city with ISO 3166-2:TW format.')
-    }
-}
+        console.error('Please specify a city with ISO 3166-2:TW format.');
+    };
+};
 
-fetchData()
+fetchData();
